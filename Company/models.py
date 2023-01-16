8c9679c46115e292a750cf08f0e67d32d3aa83b4
Ahmed <105488858+baldia275@users.noreply.github.com>
2023-01-16 04:32:52 +0100

    Update models.py

--- Company/models.py
+++ Company/models.py
@@ -1,21 +1,21 @@
 from django.core.exceptions import ValidationError
-from django.db.models import Model, TextField, DecimalField, IntegerField
+from django.db.models import Model, IntegerField, CharField
 
 
-# Create your models here.
-class Client (Model):
-    name = TextField ()
-    age = IntegerField()
-    username = TextField ()
-    address = TextField ()
-    town = TextField ()
-    country = TextField ()
+def validate_country(value):
+    if value != 'Suisse':
+        raise ValidationError('Vous devez avoir une adresse suisse pour vous inscrire')
 
-    def __str__(self):
-        return self.address
 
-    def clean(self):
-        if self.age < 18:
-            raise ValidationError ("Vous devez être majeur pour pouvoir vous inscrire.")
-        if 'Suisse' not in self.country:
-            raise ValidationError ("Vous devez avoir une adresse suisse pour vous inscrire.")
+def validate_age(value):
+    if value < 18:
+        raise ValidationError('Vous devez être majeur pour pouvoir vous inscrire.')
+
+
+class Client(Model):
+    name = CharField(max_length=15)
+    age = IntegerField(validators=[validate_age])
+    username = CharField(max_length=15)
+    address = CharField(max_length=30)
+    town = CharField(max_length=15)
+    country = CharField(max_length=15,validators=[validate_country])
